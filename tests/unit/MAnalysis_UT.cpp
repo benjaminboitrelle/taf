350b62c56a698c1096df77f490b5ab59c7712bc7
benjaminboitrelle <benjamin.boitrelle@desy.de>
2024-11-22 16:06:01 +0100

    Create unit test file

---
+++ tests/unit/MAnalysis_UT.cpp
@@ -0,0 +1,10 @@
+#define BOOST_TEST_MODULE MAnalysis_UT
+#include <boost/test/included/unit_test.hpp>
+#include <sstream>
+
+#define private public
+#include "code/include/MAnalysis.h"
+
+BOOST_AUTO_TEST_SUITE(MimosaAnalysis_UT)
+
+BOOST_AUTO_TEST_SUITE_END()
