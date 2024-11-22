da044e356590f95664d2689d787fbe9a2774e424
benjaminboitrelle <benjamin.boitrelle@desy.de>
2024-11-22 16:06:48 +0100

    Add test method CheckIfDone

--- tests/unit/MAnalysis_UT.cpp
+++ tests/unit/MAnalysis_UT.cpp
@@ -7,4 +7,39 @@
 
 BOOST_AUTO_TEST_SUITE(MimosaAnalysis_UT)
 
+BOOST_AUTO_TEST_CASE(test_CheckIfDone_false_return)
+{
+    auto analyse{MimosaAnalysis()};
+
+    analyse.fClearDone = false;
+
+    auto check_options = {
+        "clear",
+        "init",
+        "mimosall",
+        "mimosapro",
+        "mimosafakerate",
+        "mimosacalibration",
+        "mimosaminivectors",
+        "mimosapro2planes",
+        "mimosaimaging",
+    };
+
+    for (const auto &check : check_options)
+    {
+        BOOST_CHECK_EQUAL(false, analyse.CheckIfDone(check));
+    }
+}
+
+BOOST_AUTO_TEST_CASE(test_CheckIfDone_multiple_inputs)
+{
+    auto analyse{MimosaAnalysis()};
+
+    analyse.fClearDone = false;
+    analyse.fMimosaProDone = false;
+
+    auto check_options = "init,mimosapro";
+    BOOST_CHECK_EQUAL(false, analyse.CheckIfDone(check_options));
+}
+
 BOOST_AUTO_TEST_SUITE_END()
